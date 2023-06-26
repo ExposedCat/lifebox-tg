d35c930f37fbdc28962aa95ac6f48753dc0a0dc1
Artem Prokop <artem13.prokop@gmail.com>
2023-06-26 18:09:11 +0200

    Round existing dates in db

---
+++ src/migrations/round-dates.ts
@@ -0,0 +1,42 @@
+import { Database } from '../types/index.js'
+
+import { connectToDb } from '../config/index.js'
+import { loadEnv } from '../helpers/index.js'
+
+loadEnv()
+
+async function migrate(database: Database['users']) {
+	return database.updateMany({}, [
+		{
+			$set: {
+				dayRates: {
+					$map: {
+						input: '$dayRates',
+						in: {
+							$mergeObjects: [
+								'$$this',
+								{
+									date: {
+										$dateTrunc: {
+											date: '$$this.date',
+											unit: 'day'
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			}
+		}
+	])
+}
+
+console.info(`Connecting…`)
+const { database, client } = await connectToDb()
+console.info(`Running migration…`)
+const result = await migrate(database.users)
+console.info(`Result:`, JSON.stringify(result, null, 2))
+console.info(`Disconnecting…`)
+await client.close()
+console.info(`Done`)
