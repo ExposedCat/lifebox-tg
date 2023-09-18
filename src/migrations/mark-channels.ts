57e334ecd8e495fe9acba15cf69941e5ccc12b56
Artem Prokop <artem13.prokop@gmail.com>
2023-09-18 17:52:06 +0200

    Add channel polls migration

---
+++ src/migrations/mark-channels.ts
@@ -0,0 +1,23 @@
+import { Database } from '../types/index.js'
+
+import { connectToDb } from '../config/index.js'
+import { loadEnv } from '../helpers/index.js'
+
+loadEnv()
+
+async function migrate(database: Database['groups']) {
+	await database.updateMany(
+		{},
+		{
+			$set: { isChannel: false }
+		}
+	)
+}
+
+console.info(`Connecting…`)
+const { database, client } = await connectToDb()
+console.info(`Running migration…`)
+await migrate(database.groups)
+console.info(`Disconnecting…`)
+await client.close()
+console.info(`Done`)
